f79e49341a46ad9085bb5fa6d4af1902484640fe
Sirej Dua <sirejdua@gmail.com>
2019-03-20 17:16:37 -0700

    added another transpose implementation

--- examples/transpose.py
+++ examples/transpose.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def transpose(M):
     MT = np.zeros((M.shape[1], M.shape[0]), dtype=M.dtype)
@@ -8,6 +9,22 @@ def transpose(M):
 
     return MT
 
+def transpose2(M):
+    lst = []
+    for i in range(M.shape[1]):
+        lst.append(M[:,i])
+    return np.array(lst)
+
 def fast_transpose(M):
     return M.T
 
+if __name__ == '__main__':
+    M = np.random.normal(size=(20000, 20000))
+#    print(M)
+
+    if (sys.argv[1] == "2"):
+        transpose2(M)
+    elif (sys.argv[1] == "fast"):
+        fast_transpose(M)
+    else:
+        transpose(M)
