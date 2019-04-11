c00474bc65d87c48967444a15db9194ce32da680
Sirej Dua <sirejdua@gmail.com>
2019-04-11 16:21:03 -0700

    added sum, element*, element+, dot, extend, eye

--- synthesis.rkt
+++ synthesis.rkt
@@ -12,21 +12,34 @@
 ; Only needs to be done once
 ;(enable-cpyimport!)
 
-(define y (array #[#[1 2]#[3 4]]))
+(define y1 (array #[#[1 2]#[3 4]]))
 
 ; Import python module
 (py-import "prog" as python-module)
-(py-method-call python-module "transpose_py" y)
+(py-method-call python-module "transpose_py" y1)
 
 ; Syntax for DSL
-(struct transpose (arg) #:transparent)
+(struct transpose (arg)        #:transparent)
+(struct sum (arg)              #:transparent)
+(struct dot (arg1 arg2)      #:transparent)
+(struct element* (arg1 arg2) #:transparent)
+(struct element+ (arg1 arg2) #:transparent)
+(struct extend (arg1 arg2)   #:transparent)
+(struct eye (arg)              #:transparent)
+
 
 
 ; Interpreter for our DSL.
 ; We just recurse on the program's syntax using pattern matching.
 (define (interpret p)
   (match p
-    [(transpose a)  (array-axis-swap (interpret a) 0 1)]
+    [(transpose a)    (array-axis-swap (interpret a) 0 1)]
+    [(sum a)          (apply + (array->list (interpret a)))]
+    [(element* a b)   (map * (array->list (interpret a) (array->list (interpret b))))]
+    [(element+ a b)   (map + (array->list (interpret a) (array->list (interpret b))))]
+    [(dot a b)        (sum (element* (interpret a) (interpret b)))]
+    [(extend a b)     (array-list->array (list (interpret a) (interpret b)))]
+    [(eye n)          (build-array #(n n) (lambda (ids) (if (equal? (vector-ref ids 0) (vector-ref ids 1)) 1 0 )))  ]
     [_ p]))
 
 
@@ -34,17 +47,30 @@
 ; possible values.
 (define (??expr terminals)
   (define a (apply choose* terminals))
+  (define b (apply choose* terminals))
   (choose* (transpose a)
+           (sum a)
+           (element* a b)
+           (element+ a b)
+           (dot a b)
+           (extend a b)
+           ;(eye a)
            a))
 
 
 (define (make-int x)
   (define-symbolic* i integer?)
   i)
+(define (make-array n) 
+  (build-array #(3 3) make-int))
 
 ; Variables 
-(define x 
-  (build-array #(2 2) make-int))
+(define x (make-array 2))
+;(define y (make-array 2))
+;(define z (make-array 2))
+;(define a (make-int a))
+;(define b (make-int b))
+;(define c (make-int c))
 
 
 (define sketch
