013177b8982809b2d32b53d13226a120c8f263c2
Connor Prisby <connor.prisby@gmail.com>
2019-04-10 19:36:23 -0700

    Added python file

---
+++ prog.py
@@ -0,0 +1,13 @@
+#lang python
+
+# cpyimport numpy as np
+import predicates
+
+# class rkarray(np.ndarray):
+	
+
+# predicates.set_predicate(list, list)
+
+def transpose_py(n):
+	print(n)
+	# return np.transpose(n)
