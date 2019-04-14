2359dfaeace9ee6ad4db945382324dfd8f3c86ca
Sirej Dua <sirejdua@gmail.com>
2019-04-13 22:05:04 -0700

    done

--- synthesis.rkt
+++ synthesis.rkt
@@ -1,4 +1,4 @@
-#lang rosette/safe
+#lang rosette
 
 (require rosette/lib/angelic  ; provides `choose*`
          rosette/lib/match    ; provides `match`
@@ -12,7 +12,7 @@
 ; Only needs to be done once
 ;(enable-cpyimport!)
 
-(define y1 (array #[#[1 2]#[3 4]]))
+(define y1 '((1 2) (3 4)))
 
 ; Import python module
 (py-import "prog" as python-module)
@@ -25,8 +25,8 @@
 (struct element* (arg1 arg2)     #:transparent)
 (struct element+ (arg1 arg2)     #:transparent)
 ;(struct extend (arg1 arg2)       #:transparent)
-(struct matmul (arg1) (arg2)     #:transparent)
-(struct slice-col (arg1) (arg2)  #:transparent)
+(struct matmul (arg1 arg2)     #:transparent)
+(struct slice-col (arg1 arg2)  #:transparent)
 (struct eye-like (arg)           #:transparent)
 (struct eye-like-squares (arg)   #:transparent)
 (struct zeros-like (arg)         #:transparent)
@@ -38,30 +38,39 @@
 ; We just recurse on the program's syntax using pattern matching.
 
 (define (index a i j) (list-ref (list-ref a i) j))
-(define (elementwise a b op) (let a_ = (interpret a), b_ = (interpret b) in
-                             (if 
-                               (and (equals? (length a_) (length b_)) (equals? (length (car a_)) (length (car b_))))
-                               (build-list (length a_)  (lambda (i) (build-list (length (car a_)) (lambda (j) (op (index a_ i j) (index b_ i j)  ))))) 
-                               (assert #f) )))
+(define (elementwise a b op) (for/list ([i (interpret a)] [j (interpret b)]) (for/list ([a i] [b j]) (op a b))))
+;(define (elementwise a b op) (let a_ = (interpret a), b_ = (interpret b) in
+;                             (if 
+;                               (and (equals? (length a_) (length b_)) (equals? (length (car a_)) (length (car b_))))
+;                               (build-list (length a_)  (lambda (i) (build-list (length (car a_)) (lambda (j) (op (index a_ i j) (index b_ i j)  ))))) 
+;                               (assert #f) )))
 
 ; TODO use lists instead of arrays?
 (define (interpret p)
   (match p
-    [(transpose a)         (array-axis-swap (interpret a) 0 1)]
+    [(transpose a)         (apply map list (interpret a))]
     [(sum a)               (list (list (apply + (interpret a))))]
-    [(element* a b)        (elementwise a b *)]
-    [(element+ a b)        (elementwise a b +)]
+    [(element* a b)        (elementwise (interpret a) (interpret b) *)]
+    [(element+ a b)        (elementwise (interpret a) (interpret b) +)]
     ;[(dot a b)             (sum (element* (interpret a) (interpret b)))]
     ;[(extend a b)          (array-list->array (list (interpret a) (interpret b)))]
-    [(eye-like a)          (build-list (length a) (lambda (id) (build-list (length (car a)) (lambda (id2) (if (equals? id id2) 1 0)))))]
-    [(eye-like-squares a)  (build-list (length a) (lambda (id) (build-list (length a)       (lambda (id2) (if (equals? id id2) 1 0)))))]
-    [(zeros-like a)        (build-list (length a) (lambda (id) (build-list (length (car a)) (lambda (id2) 0 ))))  ]
-    [(ones-like a)         (build-list (length a) (lambda (id) (build-list (length (car a)) (lambda (id2) 1 ))))  ]
-    [(matmul a b)          (let a_ = (interpret a), b_ = (transpose (interpret b)) in
-                             (if 
-                               (equals? (length (car a_)) (length b_))
-                               (build-list (length a_) (lambda (i) (build-list (length b_) (lambda (j) (apply + (element* (list-ref a_ i) (list-ref b_ j)))))))
-                               (assert #f) ))]
+    [(eye-like a)          (let ([a_ (interpret a)])
+                             (build-list (length a_) (lambda (id) (build-list (length (car a_)) (lambda (id2) (if (equal? id id2) 1 0))))))]
+    [(eye-like-squares a)  (let ([a_ (interpret a)])
+                             (build-list (length a_) (lambda (id) (build-list (length a_)       (lambda (id2) (if (equal? id id2) 1 0))))))]
+    [(zeros-like a)        (let ([a_ (interpret a)])
+                             (build-list (length a_) (lambda (id) (build-list (length (car a_)) (lambda (id2) 0 )))))  ]
+    [(ones-like a)         (let ([a_ (interpret a)])
+                             (build-list (length a_) (lambda (id) (build-list (length (car a_)) (lambda (id2) 1 )))))  ]
+
+
+    ;    [(matmul a b)          (let a_ = (interpret a), b_ = (transpose (interpret b)) in
+;                             (if 
+;                               (equals? (length (car a_)) (length b_))
+;                               (build-list (length a_) (lambda (i) (build-list (length b_) (lambda (j) (apply + (element* (list-ref a_ i) (list-ref b_ j)))))))
+;                               (assert #f) ))]
+;    [(matmul a b)          (for/list ([i (interpret a)] [j (interpret (transpose b))]) (for/fold ([sum 0]) ([c i] [d j]) (+ sum (+ c d))))]
+    [(matmul a b)          (for/list ([i (interpret a)]) (for/list ([j (interpret (transpose b))]) (for/fold ([sum 0]) ([c i] [d j]) (+ sum (* c d)))))]
     [_ p]))
 
 
@@ -76,7 +85,6 @@
            (element+ a b)
 ;           (dot a b)
 ;           (extend a b)
-           (eye a)
            (eye-like a)
            (eye-like-squares a)
            (zeros-like a)
@@ -88,11 +96,11 @@
 (define (make-int x)
   (define-symbolic* i integer?)
   i)
-(define (make-array n) 
-  (build-array #(3 3) make-int))
+(define (make-list n) 
+  (build-list n (lambda (id) (build-list n (lambda (id2) (make-int n) )))))
 
 ; Variables 
-(define x (make-array 2))
+(define x (make-list 2))
 ;(define y (make-array 2))
 ;(define z (make-array 2))
 ;(define a (make-int a))
@@ -116,3 +124,5 @@ M
 ; Substitute the bindings in M into the sketch to get back the
 ; synthesized program.
 (evaluate sketch M)
+
+(interpret (matmul (eye-like x) x))
