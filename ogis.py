3cbae82c9b63186f6f41d2bde5df879c52502d63
Connor Prisby <connor.prisby@gmail.com>
2019-05-02 16:14:15 -0700

    Convert tabs to spaces

--- ogis.py
+++ ogis.py
@@ -4,27 +4,27 @@ import numpy as np
 # OGIS
 
 def main():
-	oracle = some_func
-	ex_set = {}
+    oracle = some_func
+    ex_set = {}
 
-	components = []
-	populate_components()
+    components = []
+    populate_components()
 
-	example = np.ones((2,2))
-
-
-	while example is not None:
-		ex_set[example] = oracle(example)
-
-		program_candidate = synthesize(ex_set, components)
-
-		if program_candidate is None:
-			#change components?
-			pass
+    example = np.ones((2,2))
 
 
-		example = find_dist_constraint(ex_set, program_candidate, components)
+    while example is not None:
+        ex_set[example] = oracle(example)
+
+        program_candidate = synthesize(ex_set, components)
+
+        if program_candidate is None:
+            #change components?
+            pass
+
+
+        example = find_dist_constraint(ex_set, program_candidate, components)
 
 
 if __name__ == '__main__':
-	main()
+    main()
