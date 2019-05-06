625b36fa70873759cae1aea79e15090bfeeb6859
Connor Prisby <connor.prisby@gmail.com>
2019-05-05 20:45:52 -0700

    Use Distinct and fix undef var

--- synthesize.py
+++ synthesize.py
@@ -6,18 +6,20 @@ class Formulate:
         self.P, self.R, phi_i = zip(*components)
         self.phi_lib = And(*[phi_i])
         self.N = len(components) # number of components
-        self.M = len(examples[0][0]) + self.N # number of formal parameters
+        self.len_I = len(examples[0][0]) # number of formal parameters
+        self.M = self.len_I + self.N
         self.L = # TODO: fill in
 
     def psi_cons(self):
-        return And(*[self.L[x] != self.L[y] for x in self.R for y in self.R if x != y])
+        # return And(*[self.L[x] != self.L[y] for x in self.R for y in self.R if x != y])
+        return Distinct(*[self.L[x] for x in self.R])
 
     def psi_acyc(self):
         return And(*[And(*[self.L[x] <= self.L[self.R[i]] for x in self.P[i]]) for i in range(self.N)])
 
     def psi_wfp(self):
         return  And(And(*[And(0 <= self.L[x], self.L[x] < self.M) for x in self.P]),
-                    And(*[And(len(self.I) <= self.L[x], self.L[x] < self.M) for x in self.R]),
+                    And(*[And(self.len_I <= self.L[x], self.L[x] < self.M) for x in self.R]),
                     self.psi_cons(),
                     self.psi_acyc())
 
